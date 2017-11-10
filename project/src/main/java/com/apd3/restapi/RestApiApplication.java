418c78bed728c0db5dc51de86a82f15da825e124
Clauber Stipkovic <clauber.halic@gmail.com>
2017-11-10 03:03:36 -0200

    Project scaffolding

---
+++ project/src/main/java/com/apd3/restapi/RestApiApplication.java
@@ -0,0 +1,12 @@
+package com.apd3.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiApplication.class, args);
+	}
+}
